128ee77b871643e07e340e89e0dc65054345c99a
youngchoKim <dudch91@naver.com>
2023-04-09 16:42:12 +0900

    change youtube api mode

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,12 +1,12 @@
 import React, { createContext, useContext } from "react";
 import { RealYoutubeApi } from "../api/RealYoutubeApi";
 import { Youtube } from "../api/Youtube";
-import { FakeYoutubeApi } from "../api/FakeYoutubeApi";
+// import { FakeYoutubeApi } from "../api/FakeYoutubeApi";
 
 const youtubeContext = createContext();
 
-const youtubeApi = new FakeYoutubeApi();
-// const youtubeApi = new RealYoutubeApi();
+// const youtubeApi = new FakeYoutubeApi();
+const youtubeApi = new RealYoutubeApi();
 const youtube = new Youtube(youtubeApi);
 
 export function YoutubeApiProvider({ children }) {
